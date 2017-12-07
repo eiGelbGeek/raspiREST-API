e9a2fd8fc485c0c6ce19f3a8dc9632800efd3c55
Kevin <kevin@pirih.de>
2017-12-07 06:02:53 +0100

    Typo Errors Fixed

--- script/raspbian_setup.sh
+++ script/raspbian_setup.sh
@@ -18,7 +18,7 @@ echo "hdmi_mode=31" >> /boot/config.txt
 #Keyboard Config
 >/etc/default/keyboard
 
-cat << 'EOF' > /etc/default/keyboard
+cat <<EOF > /etc/default/keyboard
 # KEYBOARD CONFIGURATION FILE
 
 # Consult the keyboard(5) manual page.
@@ -54,7 +54,7 @@ echo "dtoverlay=lirc-rpi,gpio_out_pin=17,gpio_in_pin=18" >> /boot/config.txt
 
 >/etc/lirc/hardware.conf
 
-cat << 'EOF' > /etc/lirc/hardware.conf
+cat <<EOF > /etc/lirc/hardware.conf
 # /etc/lirc/hardware.conf
 #
 # Arguments which will be used when launching lircd
@@ -86,7 +86,7 @@ EOF
 #Add Remotes
 >/etc/lirc/lircd.conf
 
-cat << 'EOF' > /etc/lirc/lircd.conf
+cat <<EOF > /etc/lirc/lircd.conf
 # Please make this file available to others
 # by sending it to <lirc@bartelmus.de>
 #
